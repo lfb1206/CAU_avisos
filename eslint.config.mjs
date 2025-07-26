5bb3575c7639948855f8caa99e43b423c6254261
Lucas Fernandez <lucas.fernndez@uc.cl>
2025-07-26 18:43:56 -0400

    Fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,6 @@
-// .eslintrc.js
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
+// eslint.config.mjs
 export default [
   {
-    extends: ['next/core-web-vitals'],
-    rules: {
-      'react/no-unescaped-entities': 'off'
-    }
-  }
+    ignores: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+  },
 ];
